079e31805e440dec468136e785f4a4174ccfee1e
Hannah Nguyen <hnguyen160801@gmail.com>
2023-04-28 15:16:00 -0400

    oops

--- models.py
+++ models.py
@@ -14,3 +14,46 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+
+    __tablename__ = 'pets'
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True,
+        autoincrement = True
+    )
+
+    name = db.Column(
+        db.Text,
+        nullable = False
+    )
+
+    species = db.Column(
+        db.Text,
+        nullable = False
+    )
+
+    photo_url = db.Column(
+        db.Text,
+        nullable = False,
+        default = ''
+    )
+
+    age = db.Column(
+        db.Text,
+        nullable = False
+    )
+
+    notes = db.Column(
+        db.Text,
+        nullable = True
+    )
+
+# FIXME: COMPLETE THIS
+    available = db.Column(
+        nullable = False,
+        default = True
+    )
